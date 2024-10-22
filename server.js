5605c3d8e391fbe1b3a3dd6649ef540825f15bc2
MShubar <pokeeeeepow@gmail.com>
2024-10-22 17:52:35 +0300

    first commit

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const collectibles = [
   { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
 ]
 
+const shoes = [
+  { name: 'Birkenstocks', price: 50, type: 'sandal' },
+  { name: 'Air Jordans', price: 500, type: 'sneaker' },
+  { name: 'Air Mahomeses', price: 501, type: 'sneaker' },
+  { name: 'Utility Boots', price: 20, type: 'boot' },
+  { name: 'Velcro Sandals', price: 15, type: 'sandal' },
+  { name: 'Jet Boots', price: 1000, type: 'boot' },
+  { name: 'Fifty-Inch Heels', price: 175, type: 'heel' }
+]
+
 app.use(morgan('dev'))
 
 //exercise 1
@@ -24,12 +34,20 @@ app.get('/roll/:number', (req, res) => {
 
 //exercise 3
 app.get('/collectibles/:index', (req, res) => {
-  let collectiblesName = collectibles[req.params.index].name
-  let collectiblesPrice = collectibles[req.params.index].price
-  res.send(
-    `So, you want the ${collectiblesName}? For ${collectiblesPrice}, it can be yours!`
-  )
+  const index = parseInt(req.params.index)
+
+  if (index < collectibles.length) {
+    const collectiblesName = collectibles[index].name
+    const collectiblesPrice = collectibles[index].price
+    res.send(
+      `So, you want the ${collectiblesName}? For ${collectiblesPrice}, it can be yours!`
+    )
+  } else {
+    res.send('This item is not yet in stock. Check back soon!')
+  }
 })
+
+//exercise 4
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
